3a2def822a0a3a50044e400192e27e71497ea651
Emma Willey <emma.n.willey@vanderbilt.edu>
2022-06-07 21:18:32 -0500

    File Handling and Routing

--- src/app.js
+++ src/app.js
@@ -9,8 +9,41 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, '/public/')));
 
+const accountData = fs.readFileSync('src/json/accounts.json', 'utf8');
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync('src/json/users.json', 'utf8');
+const users = JSON.parse(userData);
+
 app.get('/', (req, res) => {
-    res.render('index', {title: 'Index'});
+    res.render('index', {
+        title: 'Account Summary',
+        accounts: accounts
+    });
+});
+
+app.get('/savings', (req, res) => {
+    res.render('account', {
+        account: accounts.savings
+    });
+});
+
+app.get('/checking', (req, res) => {
+    res.render('account', {
+        account: accounts.checking
+    });
+});
+
+app.get('/credit', (req, res) => {
+    res.render('account', {
+        account: accounts.credit
+    });
+});
+
+app.get('/profile', (req, res) => {
+    res.render('profile', {
+        user: users[0]
+    });
 });
 
 app.listen(3000, () => {
